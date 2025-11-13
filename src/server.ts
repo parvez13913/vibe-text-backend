81531c73dadb5db366e6642f7422ed6d482ef96a
Parvez <parvezz13913@gmail.com>
2025-11-13 09:40:26 +0600

    [DONE] Initial setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  await mongoose.connect(config.database_url as string);
+  app.listen(config.port, () => {
+    console.log(`Server running at port ${config.port}`);
+  });
+}
+main();
